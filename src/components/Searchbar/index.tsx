8c53c7357e9ee69df46a58087d660b8be06bc051
Jesper Olsson <skoldfold@gmail.com>
2022-08-18 11:59:31 +0200

    Hide search bar on scroll down and show search bar on scroll up

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,17 +1,42 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { AppDispatch, RootState } from "../../app/store"
-import { setSearchedMemes, setShowSearchedMemes } from "../../features/memeSlice"
+import {
+  setSearchedMemes,
+  setShowSearchedMemes,
+} from "../../features/memeSlice"
 import { StyledSearchBar, Wrapper } from "./style"
 
 const Searchbar = () => {
-  const { memeData, searchedMemes, showSearchedMemes } = useSelector((state: RootState) => state.memeData)
+  const { memeData, searchedMemes, showSearchedMemes } = useSelector(
+    (state: RootState) => state.memeData
+  )
   const { showModal } = useSelector((state: RootState) => state.modal)
+  const [scrollDirection, setScrollDirection] = useState<string>("top")
   const dispatch = useDispatch<AppDispatch>()
+
+  useEffect(() => {
+    let prevValue = 0
+    window.onscroll = () => {
+      let y = window.scrollY
+      if (y > prevValue) {
+        setScrollDirection("downwards")
+      }
+      if (y < prevValue) {
+        setScrollDirection("upwards")
+      }
+      if (y === 0) {
+        setScrollDirection("top")
+      }
+      prevValue = y
+    }
+  }, [])
 
   const searchMemes = (searchterm: string) => {
     const re = new RegExp(searchterm, "i")
-    const searchedMemes = memeData.data.memes.filter((meme) => meme.name.match(re))
+    const searchedMemes = memeData.data.memes.filter((meme) =>
+      meme.name.match(re)
+    )
     dispatch(setSearchedMemes(searchedMemes))
     if (!showSearchedMemes) {
       dispatch(setShowSearchedMemes())
@@ -20,13 +45,21 @@ const Searchbar = () => {
 
   return (
     <>
-      {/* {!showModal && ( */}
-        <Wrapper showModal={showModal}>
-          <StyledSearchBar placeholder="Search for memes..." onChange={(e) => searchMemes(e.target.value)} />
-          {showSearchedMemes && searchedMemes.length !== memeData.data.memes.length && <h2>{searchedMemes.length} memes found</h2>}
-          {searchedMemes.length === 0 && showSearchedMemes ? null : <h2>Click on a meme to customize it!</h2>}
-        </Wrapper>
-      {/* )} */}
+      <Wrapper showModal={showModal}>
+        <StyledSearchBar
+          placeholder="Search for memes..."
+          onChange={(e) => searchMemes(e.target.value)}
+          scrollDirection={scrollDirection}
+          showModal={showModal}
+        />
+        {showSearchedMemes &&
+          searchedMemes.length !== memeData.data.memes.length && (
+            <h2>{searchedMemes.length} memes found</h2>
+          )}
+        {searchedMemes.length === 0 && showSearchedMemes ? null : (
+          <h2>Click on a meme to customize it!</h2>
+        )}
+      </Wrapper>
     </>
   )
 }
--- src/components/Searchbar/style.ts
+++ src/components/Searchbar/style.ts
@@ -1,21 +1,21 @@
 import styled from "styled-components"
 
-export const Wrapper = styled.div<{showModal:boolean}>`
-  display: ${(props) => (props.showModal ?  null : "flex")};
-  visibility: ${(props) => (props.showModal ?  "hidden" : "visible")};
+export const Wrapper = styled.div<{ showModal: boolean }>`
+  display: ${(props) => (props.showModal ? null : "flex")};
+  visibility: ${(props) => (props.showModal ? "hidden" : "visible")};
   flex-direction: column;
   align-items: center;
   color: white;
-  
-  h2:nth-child(2){
+
+  h2:nth-child(2) {
     margin: 7rem 0 0 0;
   }
 `
 
-export const StyledSearchBar = styled.input`
+export const StyledSearchBar = styled.input<{ scrollDirection: string; showModal: boolean }>`
   padding: 1rem;
   width: 30%;
   margin-top: 2rem;
   position: fixed;
-  `  
-
+  visibility: ${({ scrollDirection, showModal }) => ((scrollDirection === "top" && !showModal) || (scrollDirection === "upwards" && !showModal) ? "visible" : "hidden")};
+`
